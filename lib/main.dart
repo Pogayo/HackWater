12a7c7c90ccda40e776ff99f03c4535806189c8f
pogayo <pogayo17@alustudent.com>
2019-12-05 23:58:29 +0200

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,43 @@
 import 'package:flutter/material.dart';
+import 'authentication.dart';
+import 'login.dart';
+import 'root_page.dart';
 
+
+/// google green
+/// Color(0xff1aa260)
+/// yellow
+/// Color(0xffffce44)
+/// red
+/// Color(0xffde5246)
+/// blue Color(0xff4c8bf5)
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Hack Water',
       theme: ThemeData(
+        cursorColor:Colors.blue,
         primaryColor: Colors.blue,
+        accentColor: Color(0xffE7CFFF),
+        bottomAppBarColor: Color(0xff120023),
+        dividerColor:Color(0xffE7CFFF),
         primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Hack Water'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        buttonTheme:ButtonThemeData(
+            buttonColor: Colors.blue),
+        accentColorBrightness: Brightness.dark,
+        textTheme:TextTheme(
+//            headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          title: TextStyle(fontSize: 18.0, fontStyle: FontStyle.italic,color:Color(0xff120023)),
+          body1: TextStyle( fontFamily: 'Montserrat'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: new RootPage(auth: new Auth()),
     );
   }
 }
+
